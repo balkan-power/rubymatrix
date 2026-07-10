2cf980579f7202efcababff6f444e3b7fc9d541d
balkan-power <dimitar.ivanov@student.gildeopleidingen.nl>
2026-07-10 13:44:48 +0200

    Update rubymatrix.rb

--- rubymatrix.rb
+++ rubymatrix.rb
@@ -2,16 +2,16 @@
 
 # © 2026 Dimitar Ivanov (balkan-power), all rights reserved.
 
-# RubyMatrix is free software: you can redistribute it and/or modify it under the terms of the 
+# RubyMatrix is free software: you can redistribute it and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation, either version 3 of
 # the License, or any later version.
 # 
-# RubyMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# RubyMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 # 
-# You should have received a copy of the GNU General Public License along with this program. 
-# If not, see <https://www.gnu.org/licenses/>. 
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
 
 # Default constants
 version = "1.2.6"
